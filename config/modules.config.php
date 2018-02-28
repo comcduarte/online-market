a6293e98bb4f32b0664a0455e06b2bbe1938db68
duartec <duartec@IT-WKS001.MidNet.CityOfMiddletown.com>
2018-02-28 15:31:25 -0500

    Lab: Create a New Module

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,7 +11,13 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\Form',
+    'Zend\InputFilter',
+    'Zend\Filter',
+    'Zend\Hydrator',
+    'Zend\Db',
     'Zend\Router',
     'Zend\Validator',
     'Application',
+    'Market',
 ];
